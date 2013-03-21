1e2f0e4a9e1435296d993611cb921bdabf654800
Dvidi <dvidi@live.it>
2013-03-21 21:38:16 +0100

    esercizio3

--- EserciziJavascript/2013-03-14/lesson4Exercise3.js
+++ EserciziJavascript/2013-03-14/lesson4Exercise3.js
@@ -1,5 +1,15 @@
 function function_capitalizeFirstLetter(s){
-	return s.toUpperCase();
+	var primoCarattereDown = s.substring(0,1)
+	var primoCarattereUp = s.substring(0,1);
+
+	primoCarattereUp = primoCarattereUp.toUpperCase();
+
+	console.log(primoCarattereDown);
+    console.log(primoCarattereUp);
+
+	s = s.replace(primoCarattereDown,primoCarattereUp);
+
+	return s;
 }
 
 
