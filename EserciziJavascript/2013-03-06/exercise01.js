aa9a8179f4baf37ba83b10ddf9641a12b292752b
Dvidi <dvidi@live.it>
2013-03-24 15:58:29 +0100

    modifica esercizi

--- EserciziJavascript/2013-03-06/exercise01.js
+++ EserciziJavascript/2013-03-06/exercise01.js
@@ -1,3 +1,7 @@
+/*
+write a script exercise01.js that prints in console the following multiplication table, then commit it and push it:
+*/
+
 var matrix = "";
 
 for(var i=1; i<11; i++){
--- EserciziJavascript/2013-03-14/lesson4Exercise3.js
+++ EserciziJavascript/2013-03-14/lesson4Exercise3.js
@@ -1,3 +1,17 @@
+/*
+exercise03
+
+exercise03a
+
+write a function that given a word return it capitalized
+
+exercise03b
+
+write a function that capitalize each word of the following text:
+
+"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+*/
+
 function function_capitalizeFirstLetter(s){
 	var primoCarattereDown = s.substring(0,1)
 	var primoCarattereUp = s.substring(0,1);
