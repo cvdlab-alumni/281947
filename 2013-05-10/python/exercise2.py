2caadb8e12d1915e7e08c3164e2286c655576ff7
dvidi <dvidi@live.it>
2013-05-10 21:36:25 +0200

    seconda prova

---
+++ 2013-05-10/python/exercise2.py
@@ -0,0 +1,4 @@
+#Generate the 2D profile curves of the car envelope in the three coordinate directions, 
+#embed them in 3D (in the x=0, y=0 and z=0 planes, respectively, with the reference frame 
+#origin set approximately at the car centroid) and mount them together in a "two-and-a-half-dimensional" 
+#(2.5D) or "pseudo-3D" model.
